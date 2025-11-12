80ce5b23fcb014ee23fcbea30324aaffbccddc3a
Matchum designer <chaetaewon6@gmail.com>
2025-11-12 14:53:18 +0900

    Responsive AR viewer

--- src/ARViewer.js
+++ src/ARViewer.js
@@ -28,9 +28,11 @@ export default function ARViewer() {
 
   return (
     <div style={{
-       width: '100vw',
-       height: 'calc(100vh - 200px)',
-       marginTop: '70px',
+       width: '100%',
+       height: '100%',
+       position: 'absolute',
+       top: 0,
+       left: 0,
        position: 'relative', background: '#f5f5f5' }}>
       {/* model-viewer */}
       <model-viewer
